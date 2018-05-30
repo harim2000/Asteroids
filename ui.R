2456d09c896930d9df1ae14438bd3f0b1ed42497
Shuntaro Tsukazaki <shuntaro@uw.edu>
2018-05-29 22:29:34 -0700

    add the first intro paragraph and change some style

--- ui.R
+++ ui.R
@@ -8,12 +8,28 @@ shinyUI(navbarPage(theme = shinytheme("cyborg"),
   tabPanel(
     "Introduction",
     tags$h1("Asteroids Data from NASA"),
-    tags$img(src='pic1.jpg', width = "1400px", height = "800px", align = "left"),
+    tags$audio(src = "Light-Years_v001.mp3", type = "audio/mp3", autoplay = NA, controls = NA),
     
     mainPanel(
       tags$link(rel = "stylesheet", type = "text/css", href = "style.css"),
-      tags$div(class = "summary", checked = NA,
-               tags$p("About Data set")
+      tags$div(class = "data_set", checked = NA,
+               tags$h3("About Data set"),
+               tags$p("All the data to create this web page is from the 
+                      NASA JPL Asteroid team (http://neo.jpl.nasa.gov/).
+                      Specifically, we investigated NeoWs (Near Earth Object Web Service), 
+                      which is a RESTful web service for near earth Asteroid information. 
+                      NeoWs allows a user to do: search for Asteroids based on their closest 
+                      approach date to Earth, lookup a specific Asteroid with its NASA JPL 
+                      small body id, as well as browse the overall data-set. 
+                      
+                      The API was collected and is maintained by - SpaceRocks Team: 
+                      David Greenfield, Arezu Sarvestani, Jason English and Peter Baunach. 
+                      We found information about this data from aprogrammableweb, 
+                      a website that has different kinds of APIS to explore"),
+               tags$img(src='nasa.png', width = "570px", height = "450px", align = "left")
+               ),
+      tags$div(class = "about_asteroids", checked = NA,
+               tags$h3("What are asteroids")
                )
     )
   ),
