6d3e769e6212607300208ca1fcf6473dc7b3207f
Shuntaro Tsukazaki <shuntaro@uw.edu>
2018-05-29 20:37:00 -0700

    add css file and organized ui.R

--- ui.R
+++ ui.R
@@ -3,24 +3,35 @@ library(shiny)
 library(shinythemes)
 
 shinyUI(navbarPage(theme = shinytheme("cyborg"),
-                   "NASA Asteriods Data",
-  # Create a tab panel for your map
+                   "NASA Asteroids Data",
+  # tab for Introduction
   tabPanel(
     "Introduction",
-    tags$h1("Introduction")
+    tags$h1("Asteroids Data from NASA"),
+    tags$img(src='pic1.jpg', width = "1400px", height = "800px", align = "left"),
+    
+    mainPanel(
+      tags$link(rel = "stylesheet", type = "text/css", href = "style.css"),
+      tags$div(class = "summary", checked = NA,
+               tags$p("About Data set")
+               )
+    )
   ),
   
+  # tab for Distance Map
   tabPanel(
     "Distance",
     tags$h1("Distance Analysis Bubble Map"),
     plotlyOutput("Distance", height = "100%")
   ),
   
+  # tab for Static Map
   tabPanel(
     "Static Map based on Date",
     tags$h1("Static Map based on Date")
   ),
   
+  # tab for Asteroids Information
   tabPanel(
     "The asteroids as a whole",
     tags$h1("Information Tab about the asteroids as a whole")
